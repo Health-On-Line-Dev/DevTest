85b15801cadd2873cf9ef8edcc31c0355850f34a
Wayne Atkinson-Young <youngwt@gmail.com>
2020-01-14 00:04:10 +0000

    added default .net Core MVC App and test project
    initial commit after forking.
    
    The project runs on my machine and the default unit test passes

---
+++ HealthOnlone.DevTest.CurrencyViewer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HealthOnlone.DevTest.CurrencyViewer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
